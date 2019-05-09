e132bc769df876ff55fa0adaa9632ca6e54d0c9a
konradmichalak1 <konradmichalak96@gmail.com>
2019-05-09 20:26:18 +0200

    Main menu
    Added Main menu and few animation to it.

---
+++ Tower Defence/Assets/Scripts/Scenes/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string levelToLoad = "MainLevel";
+
+    public void Play()
+    {
+        EditorSceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}

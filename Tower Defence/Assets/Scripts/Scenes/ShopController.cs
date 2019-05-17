e5da999ce35eef0e1082d8d7730d60a4d132f8b4
konradmichalak1 <konradmichalak96@gmail.com>
2019-05-17 23:15:41 +0200

    Shop
    Shop scene is now scalable and display actuall values of user progress.

--- Tower Defence/Assets/Scripts/Scenes/ShopController.cs
+++ Tower Defence/Assets/Scripts/Scenes/ShopController.cs
@@ -2,31 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ShopController : MonoBehaviour {
 
+    /// <summary> Scene fader </summary>
     public SceneFader sceneFader;
+    /// <summary> Main menu scene name </summary>
     public string mainMenuLevel = "MainMenu";
+
+    /// <summary> Reference to player amount of money text </summary>
     public Text MoneyText;
 
+    [Header("Item info")]
+    /// <summary> Currently selected shop item </summary>
+    public GameObject selectedItem;
+    /// <summary> Currently selected upgrade level </summary>
+    public int selectedUpgradeLevel;
+
+
+
+
+    /// <summary> When shop item is clicked, sets reference of selectedItem and make it active. </summary>
+    /// <param name="item"></param>
+    public void SelectItem(GameObject shopItem)
+    {
+        shopItem.SetActive(true);
+        selectedItem = shopItem;
+        StartCoroutine(UpdateUITexts());
+    }
+
+
+    /// <summary> When item upgrade level is clicked, sets current upgrade level and changes values </summary>
+    /// <param name="itemLevel">Clicked game object - item level</param>
+    public void SelectItemUpgradeLevel(GameObject itemLevel)
+    {
+        //Current child index in hierarchy of 'UpgradeLevelButtons'
+        selectedUpgradeLevel = itemLevel.transform.GetSiblingIndex() + 1;
+        StartCoroutine(UpdateUITexts());
+    }
+
     /// <summary>
-    /// Returns to main menu.
+    /// Updates shown text
     /// </summary>
-	public void Back()
+    public IEnumerator UpdateUITexts()
+    {
+        yield return new WaitForSeconds(Time.deltaTime);
+        ShopTextController.itemName = selectedItem.name.Replace("Content", "");
+        ShopTextController.SetUpgradeLevel(selectedUpgradeLevel);
+        ShopTextController.UpdateTexts();
+    }
+
+
+
+
+    /// <summary> Returns to main menu. </summary>
+    public void Back()
     {
         sceneFader.FadeTo(mainMenuLevel);
     }
-    /// <summary>
-    /// FOR TESTS ONLY: RESET USER SAVED PROGRESS
-    /// </summary>
+    /// <summary> FOR TESTS ONLY: RESET USER SAVED PROGRESS </summary>
     public void ResetUserSave()
     {
         PlayerPrefs.DeleteAll();
         UpdateMoneyText();
     }
 
-    /// <summary>
-    /// Updates money text on the screen.
-    /// </summary>
+    /// <summary> Updates money text on the screen. </summary>
     private void UpdateMoneyText()
     {
         MoneyText.text = PlayerPrefs.GetInt("Money", 0).ToString() + "$";
@@ -34,14 +75,9 @@ public class ShopController : MonoBehaviour {
 
     private void Start()
     {
+        //standardTurret.SetActive(true);
+        selectedUpgradeLevel = 1;
         UpdateMoneyText();
-    }
-
-
-
-    private void Update()
-    {
-       
     }
 
 
